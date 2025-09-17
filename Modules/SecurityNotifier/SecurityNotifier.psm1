3d0a109357ec8ce613fd8db08caacc29eaffbb91
Younes-W <wimmeryounes@gmail.com>
2025-09-17 06:58:27 +0200

    Addpsm1 for SecurityNotifier module

---
+++ Modules/SecurityNotifier/SecurityNotifier.psm1
@@ -0,0 +1,32 @@
+# Load all functions from public and private directories
+
+# Public: for User accessible
+$publicParams = @{
+    Path = (Join-Path $PSScriptRoot "Public")
+    Filter = '*ps1'
+    ErrorAction = 'SilentlyContinue'
+} 
+
+$public = Get-ChildItem @publicParams
+
+foreach ($file in $public) {
+    . $file.FullName
+}
+
+# Private: only internaly, not exportable
+$privateParams = @ {
+    Path = (Join-Path $PSScriptRoot "Private")
+    Filer = '*ps1'
+    ErrorAction = SilentlyContinue
+}
+
+$private = Get-ChildItem @privateParams
+
+foreach ($file in $private) {
+    . $file.FullName
+}
+
+$exportFunctions = $public.BaseName
+Export-ModuleMember -Function $exportFunctions -Alias *
+
+
